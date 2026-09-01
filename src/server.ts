bffc2391c90285ec432a82e85b0befedfcdaa450
andresilva8624 <andresilva8624@gmail.com>
2026-08-31 22:51:55 -0300

    Updated render

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,15 @@ import { join } from 'node:path';
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
+const angularApp = new AngularNodeAppEngine({
+  allowedHosts: [
+    'localhost',
+    '127.0.0.1',
+    'm7-chamados-angular-azbb.onrender.com',
+    '*.onrender.com',
+  ],
+  trustProxyHeaders: ['x-forwarded-host', 'x-forwarded-proto', 'x-forwarded-port'],
+});
 
 /**
  * Example Express Rest API endpoints can be defined here.
@@ -41,9 +49,7 @@ app.use(
 app.use((req, res, next) => {
   angularApp
     .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
     .catch(next);
 });
 
